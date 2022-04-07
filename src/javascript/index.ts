273bf272076b996ceeaf70648aef6c991ae85029
zhangyunpeng <zhangyunpeng0126@xyz.cn>
2022-04-07 18:05:55 +0800

    init

---
+++ src/javascript/index.ts
@@ -0,0 +1,20 @@
+/**
+ * @title: index
+ * @projectName frontend-framework-simulation
+ * @description: TODO
+ * @author zhangyunpeng0126
+ * @date 2022/4/616:16
+ */
+
+type Compiler = {
+    tokenize: () => void
+    lexicalAnalyse: () => void
+}
+
+type Engine = {
+    execute:()=>void
+}
+
+type Scope = {
+
+}
